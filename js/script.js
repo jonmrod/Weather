73d4fdb0fecaf1729a24ab08dc75e11801d16549
Jonathan Rodriguez <jonathanrodriguez224@gmail.com>
2016-01-27 00:30:55 -0500

    get info from ip. yahoo api for geolocation not working

--- js/script.js
+++ js/script.js
@@ -1,54 +1,53 @@
-
 $(document).ready(function() {
-  //loadWeather(0,0);
-  navigator.geolocation.getCurrentPosition(complete, unknown);
-  /*$.getJSON('http://ipinfo.io', function(data){
-    var location=data.city+', '+data.country;
-    loadWeather(location, 0);
-    */
-    
 
-function unknown() {
-  $('#city').html("Couldn't locate. Turn on geolocation or input your location");
-}
+    getLocation();
 
-function complete(position) {
-  loadWeather(position.coords.latitude , position.coords.longitude);
-}
 })
 
-function loadWeather(location, woeid) {
-	$.simpleWeather({
-    location: '(' + location + ',' + woeid + ')',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      temp = weather.temp+'&deg;'+weather.units.temp;
-      city = weather.city+', '+weather.region;
-      icon = '<img src="' +weather.image+ '">';
-      humidity = '<center>Humidity <br>' +weather.humidity+ '%</center>';
-      description = weather.currently;
-      wind = '<center>Wind <br>' +weather.wind.direction+ ' ' +weather.wind.speed+ ' ' +weather.units.speed+'</center>';
-      lowHigh = 'L ' +weather.low+ '  H ' +weather.high;
-  
-      $("#temp").html(temp);
-      $("#city").html(city);
-      $("#icon").html(icon);
-      $("#humid").html(humidity);
-      $("#descrip").html(description);
-      $("#wind").html(wind);
-      $("#lowHigh").html(lowHigh);
+function getLocation() {
+    var location = "";
+    var zip = "";
+    $.get("http://ipinfo.io", function(response) {
+        location = response.city + ", " + response.region;
+        zip = response.postal;
+    }, "jsonp");
+    loadWeather(location, zip);
+}
 
-      forecast = '';
-      for (i = 1; i < 5; i++) {
-        forecast += '<td><center><img src="' +weather.forecast[i].thumbnail+ '"><br><b>' +weather.forecast[i].high+ '</b><br>'
-        +weather.forecast[i].low+ '<br>' +weather.forecast[i].day+ '</center></td>';
-      }
-      $("#daily").html(forecast);
-      $('td').show();
-    },
-    error: function(error) {
-      $("#city").html('<p>Error</p>');
-    }
-  });
+function loadWeather(location, zip) {
+    $.simpleWeather({
+        zipcode: "",
+        woeid: "2357536",
+        location: "",
+        unit: 'f',
+        success: function(weather) {
+            temp = weather.temp + '&deg;' + weather.units.temp;
+            city = weather.city + ', ' + weather.region;
+            icon = '<img src="' + weather.image + '">';
+            humidity = '<center>Humidity <br>' + weather.humidity + '%</center>';
+            description = weather.currently;
+            wind = '<center>Wind <br>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</center>';
+            lowHigh = 'L ' + weather.low + '  H ' + weather.high;
+
+            $("#temp").html(temp);
+            $("#city").html(city);
+            $("#icon").html(icon);
+            $("#humid").html(humidity);
+            $("#descrip").html(description);
+            $("#wind").html(wind);
+            $("#lowHigh").html(lowHigh);
+
+            forecast = '';
+            for (i = 1; i < 5; i++) {
+                forecast += '<td><center><img src="' + weather.forecast[i].thumbnail + '"><br><b>' + weather.forecast[i].high + '</b><br>' + weather.forecast[i].low + '<br>' + weather.forecast[i].day + '</center></td>';
+            }
+            $("#daily").html(forecast);
+            $('td').show();
+        },
+        error: function(error) {
+            $("#city").html('<p>Error</p>');
+            $('td').show();
+
+        }
+    });
 }
